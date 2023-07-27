0df093742ff177a402c7dce39fc44ba6311aeebd
Herale-Shreyas <98732736+Herale-Shreyas@users.noreply.github.com>
2023-07-28 01:07:04 +0530

    Create LCA_of_BST.cpp

---
+++ LCA_of_BST.cpp
@@ -0,0 +1,23 @@
+
+// Iterative Solution
+
+Node* LCA(Node *root, int n1, int n2)
+{
+   //Your code here
+   Node *temp = root;
+   if(!root)
+        return root;
+   while(temp)
+   {
+       // if both the key values are at one side of the root(temp, in this case), then traverse in that direction
+       // else it means that the key values have spilt direction indicating that the split position is the lowest ancestor
+        if(n1< temp->data && n2<temp->data)
+            temp = temp->left;
+        else if(n1>temp->data && n2>temp->data)
+            temp = temp->right;
+        else
+            return temp;
+   }
+   return root;   
+}
+
